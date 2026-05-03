43a0c64105b70ac3837c13d51abbba76be32acd3
hari2k7 <hari082007@gmail.com>
2026-05-03 10:06:31 +0530

    fix vite base path

--- PasswordGenerator/vite.config.js
+++ PasswordGenerator/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mini-Web-Projects/PasswordGenerator/',
   plugins: [
     react(),
     tailwindcss(),
